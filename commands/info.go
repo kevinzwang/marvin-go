dc35f873a179ff9c78c8b1e2b80fa268525f75a6
Kevin Wamg <20215378+kevinzwang@users.noreply.github.com>
2018-04-19 11:30:25 -0700

    better info command with embed

--- commands/info.go
+++ commands/info.go
@@ -1,12 +1,30 @@
 package commands
 
+import (
+	"strconv"
+
+	"github.com/bwmarrin/discordgo"
+)
+
 // Info gives information about the bot
 type Info struct{}
 
 func (cmd *Info) execute(ctx *Context, args []string) {
-	ctx.send("Marvin is the official administrative bot of the Chaos Discord server, written in Golang (<http://discord.gg/mXnshyA>)." +
-		"\nIf you are interested in the development of this bot, you are invited to join the official Chaos Dev server: <http://discord.gg/yvqTyGt>" +
-		"\nYou can also find all of the bot's source code here: https://github.com/kevinzwang/marvin-go")
+	guilds, _ := ctx.Session.UserGuilds(100, "", "")
+
+	embed := discordgo.MessageEmbed{
+		Title:       "About Marvin",
+		Color:       0xffffff,
+		Description: "Marvin is an all-purpose Discord bot written in Golang using the discordgo library.",
+		Fields: []*discordgo.MessageEmbedField{
+			&discordgo.MessageEmbedField{Name: "Creator", Value: "Random17#0608", Inline: true},
+			&discordgo.MessageEmbedField{Name: "Servers", Value: strconv.Itoa(len(guilds)), Inline: true},
+			&discordgo.MessageEmbedField{Name: "Dev Server", Value: "http://discord.gg/yvqTyGt", Inline: true},
+			&discordgo.MessageEmbedField{Name: "Github Repo", Value: "http://github.com/kevinzwang/marvin-go", Inline: true},
+		},
+	}
+
+	ctx.Session.ChannelMessageSendEmbed(ctx.Message.ChannelID, &embed)
 }
 
 func (cmd *Info) description() string { return "gives information about this bot" }
@@ -15,7 +33,7 @@ func (cmd *Info) category() string { return "misc" }
 
 func (cmd *Info) numArgs() (int, int) { return 0, 0 }
 
-func (cmd *Info) names() []string { return []string{"info"} }
+func (cmd *Info) names() []string { return []string{"info", "about"} }
 
 func (cmd *Info) onlyOwner() bool { return false }
 
